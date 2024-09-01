f6c4eab0911a5f0db2001768480caa4ddb122a25
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-09-01 11:03:02 -0400

    Didn't work
    Made classes, and test box, but doesn't seem to work.

---
+++ calculatorProject/calc.js
@@ -0,0 +1,15 @@
+let calcCase = document.querySelector("div");
+calcCase.style.height = "300px";
+calcCase.style.width = "200px";
+calcCase.style.color = "blue";
+/*
+for (let i=0; i<2; i++) {
+    let row = document.createElement("div.row");
+    calcCase.appendChild(row);
+    for (let i=0; i<3; i++) {
+        let cellBox = document.createElement("div.cell");
+        row.appendChild(cellBox);
+        let calcbutton = document.createElement("div.calcButton");
+        row.appendChild(calcbutton);
+    }
+}*/
